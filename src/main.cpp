c7d7a6ca55d1da363aacbd1f50d0e20ba4d3243b
csyqchen <csyqchen@comp.hkbu.edu.hk>
2026-02-02 14:27:36 +0800

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,118 +1,84 @@
 #include <iostream>
 #include <fstream>
-#include <sstream>
+#include <unordered_map>
 #include <vector>
 #include <string>
-#include <ctime>
-#include <set>
 #include "../include/Graph.h"
-#include "../include/Utils.h"
 #include "../include/Kcore.h"
-#include <chrono>
-#include <thread>
-#include <future>
 
 using namespace std;
 
-int main()
-{
-    ofstream total_output_file("test/Output_3_fp_5.txt");
+int main() {
 
     Graph* graph = new Graph();
 
-    //public graph data
-    printf("start loading public data...... ");
-    graph->load_graph("test/public_graph.txt");
-    graph->load_attribute("test/public_attribute.txt");
-    printf("public graph loaded \n");
+    std::string year = "2013";
 
-    //private graph data
-    printf("start loading private index...... ");
-    unordered_map<int, vector<PrivateEntry>> private_index;
-    load_private_file("test/test_private.txt", private_index);
-    printf("private index loaded \n");
+    cout << "Loading public graph..." << endl;
+    graph->load_graph("./" + year + "/public_graph_" + year + ".txt");
+    graph->load_attribute("./" + year + "/public_attribute_" + year + ".txt");
+    cout << "Public graph loaded." << endl;
 
-    //public graph index
     Kcore* kcore = new Kcore(graph);
-    kcore->read_attrmap("test/attribute_index.txt");
-    kcore->read_treeIndex("test/tree_index.txt");
+    kcore->read_treeIndex("./" + year + "/tree_index_" + year + ".txt");
 
-    set<Edge> tempEdges;
-    unordered_map<int, unordered_set<string>> tempAttributes;
-    std::vector<std::string> allResults;
-    for (const auto& pair : private_index)
-    {
-        int query_node = pair.first;
-        const vector<PrivateEntry>& query_private_entry = pair.second;
+    vector<int> k_list = {3};
 
-        size_t private_edge_count = query_private_entry.size();
+    for (int k : k_list) {
+        cout << "Processing k = " << k << endl;
 
-        // std::ofstream output_file("test_icde/output_5_fp_" + std::to_string(query_node) + ".txt");  //fptree
+        ofstream fp_out(
+            "./" + year + "/node_output.txt");
 
-        unordered_set<string> query_node_attributes;
-        for (const auto& item : query_private_entry) {
-            if (item.number == query_node) {
-                query_node_attributes.insert(item.tags.begin(), item.tags.end());
-            }
-        }
+        // write header
+        fp_out << "query_node\t"
+            << "time\t"
+            << "community_size\t"
+            << "attribute_size\n";
         
-        // add private attribute
-        graph->add_attributes(query_node, query_node_attributes);
+        // ofstream ab2_out(
+        //     "ICDE_revision/" + year + "/k" + to_string(k) + "/improve/ab2/one_node.txt");
 
-        const unordered_set<string>& updated_query_node_attributes = graph->attributes(query_node);
+        // query list can be read from a file if needed
+        ifstream qfile("./" + year + "/node.txt");
+        int q;
         
-        for (const auto& item : query_private_entry) {
+        while (qfile >> q) {
 
-            if (item.number == query_node) {
-                continue;
-            }
+            kcore->set_query_node(q);
+            kcore->set_k(k);
 
-            Edge e(query_node, item.number);
-            graph->addEdge(query_node, item.number);
+            // PP-FP-tree querying
+            auto [fp_time, fp_attr, fp_comm, fp_iter] =
+                kcore->fptree(
+                    "./" + year + "/index/fp_tree_output_" + to_string(q) + ".txt",
+                    "./" + year + "/output_fp_" + to_string(q) + ".txt"
+                );
 
-            unordered_set<string> filtered_attributes;
-            for (const auto& tag : item.tags) {
-                if (updated_query_node_attributes.find(tag) != updated_query_node_attributes.end()) {
-                    filtered_attributes.insert(tag);
-                }
-            }
+            fp_out << q << "\t"
+                   << fp_time << "\t"
+                   << fp_comm << "\t"
+                   << fp_attr << "\n";
 
-            graph->add_attributes(item.number, filtered_attributes);
+            // Ablation 2 (w/o public expansion)
+            // auto [ab2_time, ab2_attr, ab2_comm, ab2_iter] =
+            //     kcore->ablation2(
+            //         "ICDE_revision/2017/index_250_500/fp_tree_output_" + to_string(q) + ".txt",
+            //         "ICDE_revision/2017/k" + to_string(k) + "/improve/ab2/output_ab2_" + to_string(q) + ".txt"
+            //     );
 
-            tempEdges.insert(e);
-            tempAttributes[item.number].insert(filtered_attributes.begin(), filtered_attributes.end());
-        }
-
-
-        int k = 3;
-        printf("query node is: %d, k=%d \n", query_node, k);
-        kcore->set_query_node(query_node);
-        kcore->set_k(k);
-        
-        
-        auto [duration, max_attributes, community] = kcore->fptree("test/fp_tree_output_" + std::to_string(query_node) + ".txt",
-            "test/Output_" + std::to_string(k) + "_fp_" + std::to_string(query_node) + ".txt", allResults);  //fptree
-
-        std::ofstream total_output_file("test/Output_" + std::to_string(k) + "_fp_" + std::to_string(query_node) + ".txt", std::ios::app); 
-
-        total_output_file << "Query Duration: " << duration << " seconds \n";
-        total_output_file.flush(); 
-
-        for (const auto& edge : tempEdges) {
-            graph->deleteEdge(edge);
-        }
-        for (const auto& [node, tags] : tempAttributes) {
-            graph->remove_attribute(node, tags);
+            // ab2_out << q << "\t"
+            //         << ab2_time << "\t"
+            //         << ab2_comm << "\t"
+            //         << ab2_attr << "\t"
+            //         << ab2_iter << "\n";
         }
 
-        tempEdges.clear();
-        tempAttributes.clear();
-
-        // output_file.close();
+        fp_out.close();
+        // ab2_out.close();
     }
 
-    total_output_file.close();
+    delete kcore;
     delete graph;
-
     return 0;
 }
